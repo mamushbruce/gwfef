57a2919ff12f6dbe6a8f0152e993416e5d6fdd5a
bruce mamush <brucemamush@gmail.com>
2026-01-22 09:32:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4598934941-94f3a",
+  "appId": "1:331779098419:web:f52cd95fdf29aa7c722046",
+  "apiKey": "AIzaSyBrkwruDhT4H8UGJyINeUdrD7Q2UbqMwkM",
+  "authDomain": "studio-4598934941-94f3a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "331779098419"
+};
