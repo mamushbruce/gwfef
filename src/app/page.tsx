dfac9803b5677a89eecdf474158f539a01e6775c
bruce mamush <brucemamush@gmail.com>
2026-02-10 23:56:00 +0000

    updated-images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -177,7 +177,7 @@ export default function HomePage() {
           </div>
         </div>
       </section>
-
+{/* //dedicated */}
       <section className="py-16 md:py-24">
         <div className="container mx-auto px-4">
           <div className="text-center">
